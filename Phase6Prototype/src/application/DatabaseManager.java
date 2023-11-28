b9ad1c634163bce99c4cfe312d9a1f67f3188490
hunter-funk <hunter.d.funk@gmail.com>
2023-11-28 09:18:54 -0700

    Final Project

--- Phase6Prototype/src/application/DatabaseManager.java
+++ Phase6Prototype/src/application/DatabaseManager.java
@@ -3,11 +3,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class DatabaseManager {
 
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/Library";
-    private static final String USERNAME = "hunterfunk";
-    private static final String PASSWORD = "Nugget10f!";
+    private static final String JDBC_URL = "";
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
